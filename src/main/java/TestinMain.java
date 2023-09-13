cbe492902f5f8a619c3a469d4b41194025a9001f
texan <texan@192.168.1.9>
2023-09-13 14:38:56 +0530

    commiting main folder pgms

---
+++ src/main/java/TestinMain.java
@@ -0,0 +1,12 @@
+
+public class TestinMain {
+	
+	public static void main() {
+		
+		System.out.println("Hello from main folder");
+		
+	}
+	
+	
+
+}
---
+++ src/main/resources/TestinResources.java
@@ -0,0 +1,15 @@
+
+public class TestinResources {
+
+	
+		
+		public static void main() {
+			
+			System.out.println("Hello from main resources - this is dummy class");
+			
+		}
+		
+		
+
+	
+}
