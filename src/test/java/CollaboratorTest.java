71ac35516e1816fe7ba26d6f7df2bd6f17a3df49
texan <texan@192.168.1.9>
2023-09-13 15:49:42 +0530

    collaborator commit

---
+++ src/test/java/CollaboratorTest.java
@@ -0,0 +1,10 @@
+
+public class CollaboratorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Additional method to check collaborator access and review by mgr");
+
+	}
+
+}
